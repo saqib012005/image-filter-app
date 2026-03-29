4439b28f087acbc2c7337470199f894867e40bb8
saqib012005 <sb4961471@gmail.com>
2026-03-29 15:51:05 +0530

    Rename app.py to application.py

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ import os
 import cv2
 from filters import apply_filter
 
-app = Flask(__name__)
+application = Flask(__name__)   # ✅ FIXED
 
 UPLOAD_FOLDER = "uploads"
 OUTPUT_FOLDER = "outputs"
@@ -11,28 +11,28 @@ OUTPUT_FOLDER = "outputs"
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 os.makedirs(OUTPUT_FOLDER, exist_ok=True)
 
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['OUTPUT_FOLDER'] = OUTPUT_FOLDER
+application.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+application.config['OUTPUT_FOLDER'] = OUTPUT_FOLDER
 
 
-@app.route('/')
+@application.route('/')
 def home():
     return render_template('index.html')
 
 
-@app.route('/upload', methods=['POST'])
+@application.route('/upload', methods=['POST'])
 def upload():
     file = request.files['image']
     filter_name = request.form['filter']
 
     if file:
-        filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        filepath = os.path.join(application.config['UPLOAD_FOLDER'], file.filename)
         file.save(filepath)
 
         image = cv2.imread(filepath)
         filtered = apply_filter(image, filter_name)
 
-        output_path = os.path.join(app.config['OUTPUT_FOLDER'], "output.png")
+        output_path = os.path.join(application.config['OUTPUT_FOLDER'], "output.png")
         cv2.imwrite(output_path, filtered)
 
         return render_template('index.html', output_image="output.png")
@@ -40,13 +40,7 @@ def upload():
     return redirect(url_for('home'))
 
 
-@app.route('/download')
+@application.route('/download')
 def download():
-    path = os.path.join(app.config['OUTPUT_FOLDER'], "output.png")
+    path = os.path.join(application.config['OUTPUT_FOLDER'], "output.png")
     return send_file(path, as_attachment=True)
-
-
-import os
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
